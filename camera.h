29fc68a23fee063bd7a7d9dc2a144310b9344ff2
r-salogubov <r.salogubov@seraphim.online>
2019-11-27 17:01:40 +0300

    wip

---
+++ camera.h
@@ -0,0 +1,15 @@
+//
+// Created by rsalogub on 27.11.19.
+//
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+
+class Camera
+{
+
+};
+
+
+#endif //CAMERA_H
--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 //
 // Created by rsalogub on 26.11.19.
 //
+#include <iostream>
 #include "FRMath/vector2.h"
+#include "FRMath/matrix4.h"
 #include "FRRenderer/model.h"
+#include "FRRenderer/renderer.h"
 #include "renderer.h"
 
 int main()
 {
-    FRMath::Vector2 vector2;
-    FRRenderer::Model model("", FRMath::Vector2(0,0));
+    FRMath::Vector2<FRMath::pointType> vector2;
+    FRRenderer::Model model("", FRMath::Vector2<FRMath::pointType>(0,0));
+    FRRenderer::Renderer::getRenderer();
     return 0;
 }
